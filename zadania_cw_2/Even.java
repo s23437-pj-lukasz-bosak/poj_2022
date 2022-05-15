54a14fa252a5acc639a362d6ed5dba41d703b816
s23437-pj-lukasz-bosak <73651774+s23437-pj-lukasz-bosak@users.noreply.github.com>
2022-05-15 14:58:14 +0200

    Add files via upload

---
+++ zadania_cw_2/Even.java
@@ -0,0 +1,35 @@
+package zadanka.domowe;
+
+import java.util.Scanner;
+ // zadanie 2.14
+public class Even {
+
+    int ifEven;
+
+
+    public void dataInput() {
+        Scanner skaner = new Scanner(System.in);
+        System.out.println("Podaj Dowolną liczbe: ");
+        ifEven = skaner.nextInt();
+        System.out.println("twoja liczba: " + ifEven);
+    }
+
+    public boolean dataProcessing() {
+        if(ifEven%2 == 0){
+            return true;
+             }else {
+            return false;
+        }
+    }
+
+    public void output(){
+        if(dataProcessing() == true){
+            System.out.println(ifEven + " Jest parzysta");
+        }else{
+            System.out.println(ifEven + " Jest nieparzysta");
+        }
+    }
+
+
+
+}
